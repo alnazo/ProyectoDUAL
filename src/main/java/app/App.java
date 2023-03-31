e058c785ede28eee19f4df27d5d0cd86b3b8803d
Antonio De Llamas <a2llamas@gmail.com>
2023-03-31 12:11:49 +0200

    Creada rama de trabajo individual y cambios en APP

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -3,5 +3,6 @@ package app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Prueba de rama independiente");
     }
 }
